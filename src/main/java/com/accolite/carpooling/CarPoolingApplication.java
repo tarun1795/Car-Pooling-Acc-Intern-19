aa3322ce253787e0a6b00df5d118fa7ffd5db699
tarun <tarun.k@accoliteindia.com>
2019-01-25 16:26:37 +0530

    init

---
+++ src/main/java/com/accolite/carpooling/CarPoolingApplication.java
@@ -0,0 +1,14 @@
+package com.accolite.carpooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPoolingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPoolingApplication.class, args);
+	}
+
+}
+
